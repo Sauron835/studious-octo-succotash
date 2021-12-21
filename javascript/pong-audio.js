7091204172954b854cb6b2fa34ad63e584452911
Ryan Peschke <96027042+Sauron835@users.noreply.github.com>
2021-12-21 15:46:14 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("tap-resonant.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("tap-percussive.mp3");
+export var scoreSound = new soundFile("spacePongWall.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("Adventure_Meme.mp3");
