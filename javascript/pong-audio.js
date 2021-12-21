31f4b6a5cf5be216007ae7794b7fee120256f3fb
Ryan Peschke <96027042+Sauron835@users.noreply.github.com>
2021-12-21 15:22:13 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -8,7 +8,7 @@ class soundFile {
   constructor(file, deferPlay) {
     //this.deferPlay = false;
     this.player = new Tone.Player({
-      url: "https://housercaspar.github.io/apple-eater/sounds/" + file,
+      url: "https://github.com/Sauron835/studious-octo-succotash/tree/main/sounds" + file,
       loop: false,
       autostart: false
     }).toMaster();
