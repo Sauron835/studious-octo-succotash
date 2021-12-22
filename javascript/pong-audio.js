2fce4f36dba3bdd78c9ca1e45cb8fe12d4dcbc6a
Ryan Peschke <96027042+Sauron835@users.noreply.github.com>
2021-12-22 16:43:21 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,12 +52,12 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("spacePongWall.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("Adventure_Meme.mp3");
+export var ambientSound = new soundFile("cinematic-space-drone-10623.mp3.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("Eight_Bit_Village_Loop.mp3");
+export var adventureMusic = new soundFile("cinematic-space-drone-10623.mp3.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
